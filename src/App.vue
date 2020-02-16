33e645cd9573b6d1529d972dd4c5a50dd836ae55
sean <me@eva>
2020-02-15 18:28:05 -0600

    Add v-hotkey demo to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
       <h1>
         {{ message }}
       </h1>
+      <span v-hotkey="keymap" v-show="show"> Press `ctrl + esc` to toggle me! Hold `enter` to hide me! </span>
     <Counter/>
   </div>
 </template>
 
 <script>
 import Counter from './components/Counter.vue'
+
 export default {
   components: {
    Counter
@@ -16,8 +18,23 @@ export default {
   data() {
     return {
       message: 'Hello World',
+      show: true,
     };
   },
+  computed: {
+    keymap() {
+      return {
+        "ctrl+del": this.test,
+        "ctrl+a": this.test,
+        "s": this.test
+      };
+    }
+  },
+  methods: {
+    test() {
+      alert('sheesh')
+    }
+  }
 };
 </script>
 
