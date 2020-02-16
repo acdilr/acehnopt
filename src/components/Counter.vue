ce2e02575d145369b478b16e2354002229b052b9
sean <me@eva>
2020-02-15 18:28:12 -0600

    Add v-hotkey controls to Counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <span v-hotkey="keymap">
+      a and d to control
+    </span>
   <button v-on:click="count++">
     You clicked me {{ count }} times.
   </button> </div>
@@ -12,6 +15,24 @@ export default {
       count: 0
     };
   },
+  computed: {
+    keymap() {
+      return {
+        "left": this.decrease,
+        "a": this.decrease,
+        "d": this.increase,
+        "right": this.increase,
+      }
+    }
+  },
+  methods: {
+    decrease() {
+      this.count -= 1
+    },
+    increase() {
+      this.count += 1
+    }
+  }
 };
 </script>
 
