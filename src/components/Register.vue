b6107631dd91eb11bf4184af60f58244eaa4d40b
sean <me@eva>
2020-02-17 22:29:52 -0600

    Wahoo. lots of stuff, again. The color/styling on the register outputs is demoing two approaches.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,26 +4,29 @@
       <button v-shortkey="['a']" @shortkey='decrease()' v-on:click='decrease()'>-</button>
       {{ count }}
       <button v-shortkey="['d']" @shortkey='increase()' v-on:click="increase()">+</button>
+      <button v-on:click="init()">Init</button>
     </div>
-    <span>▓</span>
-    <span>░</span>
-    <span>░</span>
-    <span>░</span>
-    <hr>
     <div class="registers" id="trivium">
-      <span v-for="register in registers">
-        <span v-for="ff in register">
-          <span v-if="ff">▓</span>
+      <span :class="test[index]" v-for="(register, index) in registers">
+        <span v-bind:id="test[index] + '-' + ffIndex" v-for="(ff, ffIndex) in register">
+          <span  v-if="ff">▓</span>
           <span v-else>░</span>
         </span>
         <br>
       </span>
-    </div>
-      <span v-for="ff in flipflops">
-        <span v-if="ff">▓</span>
-        <span v-else>░</span>
+
+      <span id="abc" class="output" v-for="(output, index) in registerOutputs">
+        <div v-bind:id="test[index] + '-output'">
+          <span :id="output" v-if="output">▓</span>
+          <span v-else>░</span>
+        </div>
       </span>
-      <hr>
+      </span>
+        <br>
+        <p><p>
+<hr>
+    </div>
+    <hr>
     <div class="registers">
       <span v-for="bit in output">
         <span v-if="bit">▓</span>
@@ -42,71 +45,117 @@ export default {
     registers[1] = Array(84).fill(0)
     registers[2] = Array(111).fill(0)
     registers[0][0] = 1
+    registers[0][1] = 1
+    registers[0][2] = 1
+    for (let i = 3; i < 20; i++) {
+      registers[1][i] = 1
+    }
     return {
       count: 0,
       registers,
-      flipflops: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      taps: [0, 0, 0, 0,
-             0, 0, 0, 0,
-             0, 0, 1, 0,
-             1, 1, 0, 1],
       output: [],
+      registerOutputs: [],
+      test: ["register-a", "register-b", "register-c"]
     };
   },
   methods: {
     decrease() {
       this.count -= 1
     },
-    increase() {
-      this.count += 1
-      // We're going to follow a simple enough plan
-      //  One that is Vessel oriented, rather than Passage oriented
-            // [Each flipflop, not each bit in the flipflop.]
-      //  and One that is Buffered, rather than Deltaed.
-            // cf. [this.registers = registers]
-            //     [this.registers.update(delta)]
-      // Deltas are sparse, Buffers are dense, &c. &c.
+    init() {
+      this.increase(1152 - this.count)
+      this.output = []
+    },
+    increase(count=1) {
+      for (let n=0; n < count; n++) {
+        this.count += 1
+        // We're going to follow a simple enough plan
+        //  One that is Vessel oriented, rather than Passage oriented
+              // [Each flipflop, not each bit in the flipflop.]
+        //  and One that is Buffered, rather than Deltaed.
+              // cf. [this.registers = registers]
+              //     [this.registers.update(delta)]
+        // Deltas are sparse, Buffers are dense, &c. &c.
 
-      // Anyways, the plan:
-      //   Every feedforward fires,
-      //   then we handle the Trivium feeds,
-      //   and the output
-      var registers = []
-      var val, sourceRegister, sourceFlipflop
-      for (let registerIndex = 0; registerIndex < this.registers.length; registerIndex++) {
-        var register = Array()
-        for (let ffIndex in this.registers[registerIndex]) {
-          if (ffIndex == 0) {
-            val = 0
-            sourceRegister = Math.abs((registerIndex + this.registers.length - 1) % this.registers.length)
-            sourceFlipflop = this.registers[sourceRegister].length - 1
-            val ^= this.registers[sourceRegister][sourceFlipflop]
-          } else {
-            sourceRegister = registerIndex
-            sourceFlipflop = ffIndex - 1
-            val = this.registers[sourceRegister][sourceFlipflop]
+        // Anyways, the plan:
+        //   Every feedforward fires,
+        //   then we handle the Trivium feeds,
+        //   and the output
+        var registers = []
+        var val, sourceRegister, sourceFlipflop
+        // shrug. let's get the three register outputs first.
+        var outputs = [0, 0, 0]
+        for (let registerIndex = 0; registerIndex < this.registers.length; registerIndex++) {
+          var register = Array()
+          for (let ffIndex in this.registers[registerIndex]) {
+            if (ffIndex == 0) {
+              // Sanity checks for this gnarly bit:
+              //  Each output is fed by its own register
+              //  Each register input is fed by
+              //     - the preceding output, and
+              //     - one of its flipflops
+              if (registerIndex == 0) {
+                outputs[2] ^= (this.registers[2][108] & this.registers[2][109])
+                outputs[2] ^= this.registers[2][110]
+                outputs[2] ^= this.registers[2][65]
+                val = outputs[2] ^ this.registers[0][68]
+              } else if (registerIndex == 1) {
+                // The first (as in 1) flipflop on Register B takes:
+                //   - the 78th (remember the 1-index) flipflop in B
+                //   - the output of Register A, which is as follows:
+                //      = the 66th (remember the 1-index) flipflop of A
+                //      = antepenultimate & penultimate flipflop of A (yes, a bit and)
+                //      = the ultimate flipflop of A. # 93 in a 1-index.
+                outputs[0] ^= (this.registers[0][90] & this.registers[0][91])
+                outputs[0] ^= this.registers[0][92]
+                outputs[0] ^= this.registers[0][65]
+                val = outputs[0] ^ this.registers[1][77]
+              } else if (registerIndex == 2) {
+                outputs[1] ^= (this.registers[1][81] & this.registers[1][82])
+                outputs[1] ^= this.registers[1][83]
+                outputs[1] ^= this.registers[1][68]
+                val = outputs[1] ^ this.registers[2][86] // let's go with coincidence. sure.
+              }
+              //sourceRegister = Math.abs((registerIndex + this.registers.length - 1) % this.registers.length)
+              //sourceFlipflop = this.registers[sourceRegister].length - 1
+              //val ^= this.registers[sourceRegister][sourceFlipflop]
+            } else {
+              sourceRegister = registerIndex
+              sourceFlipflop = ffIndex - 1
+              val = this.registers[sourceRegister][sourceFlipflop]
+            }
+            register.push(val)
           }
-          register.push(val)
+          registers.push(register)
         }
-        registers.push(register)
+        // xor the register outputs for the net.
+        this.output.push(outputs.reduce((acc, val) => acc ^ val, 0)) // sorry
+
+        this.registers = registers
+        this.registerOutputs = outputs
       }
-      // being careful to use the last state to update the new state
-      registers[0][0] ^= this.registers[0][68]
-      registers[1][0] ^= this.registers[1][77]
-      registers[2][0] ^= this.registers[2][86]
-      this.output.push(this.flipflops[this.flipflops.length - 1])
-
-      this.registers = registers
-
     }
   }
 };
 </script>
 
 <style>
+#register-a-output, #register-a-65, #register-a-90, #register-a-91, #register-a-92 {
+  color: red;
+  /* float: right; */
+}
+.register-b {
+  color: green;
+  /* float: right; */
+}
+.register-c {
+  color: blue;
+  /* float: right; */
+}
 .registers {
   font-size: 10px;
   word-wrap: anywhere;
   width: 1000px;
+  float: left;
 }
 </style>
